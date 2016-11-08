b046f10ce72b564eabacf00bcd85b74ed7f2d482
ldh <li591554358@gmail.com>
2016-11-08 21:40:53 +0800

    change mirror to China

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -35,8 +35,9 @@
 (require 'cl)
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;             '("melpa" . "https://melpa.org/packages/") t)
+
 ;; set package-user-dir to be relative to Prelude install path
 (setq package-user-dir (expand-file-name "elpa" prelude-dir))
 (package-initialize)
---
+++ personal/preload/mirror.el
@@ -0,0 +1,4 @@
+(setq package-archives 
+       '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+	 ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+	 ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
