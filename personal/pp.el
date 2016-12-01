075589d6c1b90f402f488d8d4aaa2203b241c111
ldh <li591554358@gmail.com>
2016-12-01 20:34:52 +0800

    uncomment helm

--- personal/pp.el
+++ personal/pp.el
@@ -1,5 +1,13 @@
-;;;remove scroll bar
+;;; remove scroll bar
 (scroll-bar-mode -1)
 
-;;;install ag
+;;; search tool: ag
 (prelude-require-package 'ag)
+
+;;; yasnippet
+(prelude-require-package 'yasnippet)
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(provide 'pp)
+;;; pp.el ends here
